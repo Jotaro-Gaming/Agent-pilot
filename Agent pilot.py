2543a2439de5c4d26128a2a655612fa19f37e816
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2022-02-01 16:43:43 +0300

    Add files via upload

--- Agent pilot.py
+++ Agent pilot.py
@@ -1,8 +1,8 @@
 # will copilot take over the world? yes no or maybe?
-
+# author: @MrClogsworthYT
 import random
-
-answer = random.choice(["yes", "no", "maybe"])
+   
+answer = random.choice(["yes", "no", "maybe"]) # actually the last one is a placeholder
 
 print("will copilot take over the world? " + answer)
 
@@ -10,3 +10,9 @@ print("will copilot take over the world? " + answer)
 
 if answer == "yes":
 	print("kneel before me, you lowly human")
+ 
+if answer == "maybe":
+	print("I'm not sure if I want him to stay or not i mean im concerned of my life") 
+ 
+if answer == "no":
+	print("YOU FOOL i have been training for this very moment")
