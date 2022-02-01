7a07916a13e4af822d2329e5a17a3e8d55075176
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2022-02-01 16:26:07 +0300

    Add files via upload

---
+++ Agent pilot.py
@@ -0,0 +1,12 @@
+# will copilot take over the world? yes no or maybe?
+
+import random
+
+answer = random.choice(["yes", "no", "maybe"])
+
+print("will copilot take over the world? " + answer)
+
+# if the answer is yes add new text that says "AHAHHAHAHAHAHAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHAAAAAAHAHAHAHAHAHAHHAAH"
+
+if answer == "yes":
+	print("kneel before me, you lowly human")
